9e5702e1b24f600b93406552cd46c7668de6acd2
Mark Urquhart <markurquhart@Marks-Mac-Air-M2.local>
2023-06-22 12:39:55 -0400

    Connection config and a hello world

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 import os
 from dotenv import load_dotenv
+from klaviyo_api import KlaviyoAPI
 
 load_dotenv()
 
 # Setup API credentials - using env variables locally
 pub_key = os.getenv('PUB-API-KEY')
 pri_key = os.getenv('PRI-API-KEY')
+
+# Connection config
+klaviyo = KlaviyoAPI(pri_key, max_delay=60, max_retries=3, test_host=None)
+
+# Hello world to print basic metrics, no filtering
+print(klaviyo.Metrics.get_metrics())
