df2ae7f6ffeef3b0f48bd811f4ba6f469d59d5de
Mark Urquhart <markurquhart@Marks-Mac-Air-M2.local>
2023-06-22 12:22:55 -0400

    Establishing repo

---
+++ app.py
@@ -0,0 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+# Setup API credentials - using env variables locally
+pub_key = os.getenv('PUB-API-KEY')
+pri_key = os.getenv('PRI-API-KEY')
