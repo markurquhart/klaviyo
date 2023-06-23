541e6dd1a4a075df27ae3161bc1ef010d3cb494e
Mark Urquhart <markurquhart@marksmacairm2.lan>
2023-06-22 23:10:21 -0400

    Added Webhook Post after payload collection

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 import os
+import requests 
+import json
+
 from dotenv import load_dotenv
 from klaviyo_api import KlaviyoAPI
 
@@ -7,17 +10,24 @@ load_dotenv()
 # Setup API credentials - using env variables locally
 pub_key = os.getenv('PUB-API-KEY')
 pri_key = os.getenv('PRI-API-KEY')
+webhook = os.getenv('WEBHOOK-URL')
 
 # Connection config
 klaviyo = KlaviyoAPI(pri_key, max_delay=60, max_retries=3, test_host=None)
 
 # Hello world to print basic metrics, no filtering
-Hello_World = klaviyo.Metrics.get_metrics()
+# Hello_World = klaviyo.Metrics.get_metrics()
 
 # Filtering Example on profiles between two timestamps 
 Filter_Profiles = klaviyo.Profiles.get_profiles(
     filter='less-than(updated,2023-06-22T00:00:00Z),greater-than(updated,2023-06-01T00:00:00Z)'
     )
-print(Filter_Profiles)
+
+# Format to JSON to ready for post
+json_data = json.dumps(Filter_Profiles)
+
+
+# Post payload from Klaviyo to a webhook using webhook.site
+response = requests.post(webhook, data = json_data)
 
 
